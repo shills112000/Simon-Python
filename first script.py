f65ddf6ffd55bbf4e0d54f427839b0ea0bbc26a6
Simon Hills <simonhills11@gmail.com>
2018-02-26 14:24:55 +0000

    added more crap'
    exit
    quit
    '

--- first script.py
+++ first script.py
@@ -5,3 +5,11 @@ mystring[2:] # : to end of string
 mystring[:3]  # got upto 3rd letter
 mystring[3:6] # l to w
 mystring[::-1]  # reserves the string  
+last_letters=mystring[1:]
+'P' + last_letters
+x="Hello World"
+x= x + " it is beautful outside"
+x.upper()  # change case of upper
+x.lower() # change to lower case
+x.split() # split hello world on white space.
+x.split('i') # split on i
