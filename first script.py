75aa96876670a64d139a158d63e40699445a8ab5
Simon Hills <simonhills11@gmail.com>
2018-02-26 12:57:48 +0000

    added string indexing and splicing

--- first script.py
+++ first script.py
@@ -1,1 +1,7 @@
 print ('hello world1')
+mystring="Hello world"
+mystring[2] # imdex 0 , starts at 0 
+mystring[2:] # : to end of string
+mystring[:3]  # got upto 3rd letter
+mystring[3:6] # l to w
+mystring[::-1]  # reserves the string  
