34d02be968cfcbc25dd496ae4764b3f20f35f871
Simon Hills <simonhills11@gmail.com>
2018-03-27 10:28:29 +0100

    updated logfile_pull

--- LOGFILE_PULL_SCRIPT.py
+++ LOGFILE_PULL_SCRIPT.py
@@ -6,6 +6,8 @@ Created on Fri Mar 16 10:08:31 2018
 """
 #!/usr/bin/env python
 
+import sys, os
+
 env_name=[]
 env_jboss=[]
 file = open ('ENVS',"r")
@@ -16,7 +18,6 @@ for line in file:
 
 print ("Please choose the number below for the enviroment you wish to collect the log files from :\n")
 x=0
-type (x)
 for entry in env_name:
         print ("{0}. {1} " .format(x+1,env_name[x]))
         x+=1
@@ -26,3 +27,16 @@ result = input ('enter a number: ')
 while result < 1 or result > length:
         print ("Number needs to be inbetween 1 and {0}" .format(length))
         result = input ('enter a number: ')
+
+enviroment=env_name[result-1]
+hostname=env_jboss[result-1]
+
+print (result)
+print ("You Choose: {0} and the server is {1}" .format(enviroment, hostname))
+
+
+# rstrip is like perl chomp
+hostname=hostname.rstrip("\n")
+
+cmd = "ssh " + hostname + " date"
+os.system(cmd)
