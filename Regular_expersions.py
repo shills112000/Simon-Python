92c7ad92c294466ddaedca13a68121d66d5cd430
Simon Hills <simonhills11@gmail.com>
2018-06-18 11:14:01 +0100

    added html

---
+++ Regular_expersions.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 27 10:52:50 2018
+
+@author: SHills
+"""
+
+import re
+
+
+name = ["ddcit", "dick", "cltclit", "walker", "clitwalker","cli"]
+
+# Match names.
+for element in name:
+     print (element)
+     m=p.match('c')
+     print (m.group())
+       
+
+#p = re.compile('clit')
+
+#m = p.match("clit")
+
+#print (m.group())
+
